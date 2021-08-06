fd3a2209014544a1628decb7f1a291f8cb0235d6
ibepurplee <68716940+ibepurplee@users.noreply.github.com>
2021-08-06 08:17:56 -0700

    Add files via upload

--- assinator.py
+++ assinator.py
@@ -1,9 +1,13 @@
+import pathlib
 import PIL.Image
 import time
+import os
+
+
 
 asciiChar = ['@', '#', 'S', '%', '?', '*', '+', ';', ':', ',', '.']
 
-def resize(image, newWidth = 100):
+def resize(image, newWidth = 150):
     width, height = image.size
     ratio = height/width
     newHeight = int(newWidth * ratio * 0.6)
@@ -19,8 +23,8 @@ def assination(image):
     chars = "".join([asciiChar[pixel//25] for pixel in pixels])
     return(chars)
 
-def main(newWidth=100):
-    path = input("Enter location to image: ")
+def main(newWidth=150):
+    path = input("Enter location to image:   ")
     try:
         image = PIL.Image.open(path)
     except:
@@ -30,10 +34,21 @@ def main(newWidth=100):
     assciImg = '\n'.join(newImg[i:(i+newWidth)] for i in range(0, pixel_count, newWidth))
     print(assciImg)
 
+def countFrames():
+    count=0
+    for path in pathlib.Path("./data").iterdir():
+        if path.isFile():
+            count+=1
+    return count
 
-def assify(newWidth=100):
+fps = int(input("fps: "))
+frames = float(1/fps)
+
+
+
+def assify(newWidth=150):
     i=0
-    while i<6570:
+    while i<4000:
         path = "data/frame" + str(i) + ".jpg"
         image = PIL.Image.open(path)
         newImg = assination(grayAss(resize(image)))
@@ -41,8 +56,9 @@ def assify(newWidth=100):
         assciImg = '\n'.join(newImg[i:(i+newWidth)] for i in range(0, pixel_count, newWidth))
         print(assciImg)
         i+=1
-        time.sleep(0.04166)
-    print(i)
-    
+        time.sleep(frames)
+        os.system("clear")
 
+    
+os.system('resize -s 100 46')
 assify()
